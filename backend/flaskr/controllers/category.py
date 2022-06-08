fef7816052bf6e9e7b77d176220ce6864ef1adac
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-08 23:01:59 +0100

    Fetch Categories

--- backend/flaskr/controllers/category.py
+++ backend/flaskr/controllers/category.py
@@ -1,6 +1,16 @@
 from flaskr.controllers import categories_controller
+from models import Category
 
 
 @categories_controller.route('/')
-def index():
-    return "Blueprint is working for categories"
+def fecth_categories():
+    """Fetch all categories
+
+    Returns:
+        dict: a key-value {'id':'type'}
+    """
+    all_categories = Category.query.all()
+    return {
+        Category.format(category)['id']:  Category.format(category)['type']
+        for category in all_categories
+    }
