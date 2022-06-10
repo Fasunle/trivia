c680f3ed3c818c8d44fd1e52b7c815d470b33559
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-10 19:04:34 +0100

    Fetch questions by category

--- backend/flaskr/controllers/category.py
+++ backend/flaskr/controllers/category.py
@@ -1,5 +1,8 @@
+from flask import jsonify
+
 from flaskr.controllers import categories_controller
 from models import Category
+from models import Question
 
 
 @categories_controller.route('/')
@@ -14,3 +17,41 @@ def fecth_categories():
         Category.format(category)['id']:  Category.format(category)['type']
         for category in all_categories
     }
+
+
+@categories_controller.route('/<int:id>/questions')
+def get_by_category(id):
+    """Get all questions given a category id
+
+    Args:
+        id (int): category id
+
+    Returns:
+        result: {
+            "questions": questions,
+            "current_category": current_category,
+            "total_questions": total_questions
+        }
+    """
+
+    # get the category type
+    current_category = Category.format(Category.query.get(id)).get("type")
+
+    # get all questions in the category
+    questions = Question.query.filter_by(category=id).all()
+
+    # get the questions count
+    total_questions = Question.query.filter_by(category=id).count()
+
+    # format the questions
+    questions = [
+        Question.format(question)
+        for question in questions
+    ]
+
+    result = {
+        "questions": questions,
+        "current_category": current_category,
+        "total_questions": total_questions
+    }
+    return jsonify(result)
