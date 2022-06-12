e5065f8a32f3e5a3652a83a9847110e9c66274cf
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-12 02:41:55 +0100

    generate a random integer

--- backend/flaskr/controllers/__init__.py
+++ backend/flaskr/controllers/__init__.py
@@ -1,4 +1,9 @@
 from flask import Blueprint
+from random import randint
 
 categories_controller = Blueprint("categories", __name__)
 question_controller = Blueprint("questions", __name__)
+
+
+def get_random_integer(length=0):
+    return randint(0, length - 1)
