3a671bc06cb45e43473c1ac16885457094e2c3df
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-18 08:06:05 +0100

    config: configure the application

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,8 @@
-ERROR_OUT = True
-QUESTIONS_PER_PAGE = 10
-MAX_QUESTIONS_PER_PAGE = 10
-SQLALCHEMY_DATABASE_URI = "postgresql://Fasunle@localhost:5432/trivia"
-ALLOWED_LIST = ["http://localhost:3000"]
+import os
+
+ALLOWED_LIST = os.environ.get("ALLOWED_LIST") or ["http://localhost:3000"]
+ERROR_OUT = os.environ.get("ERROR_OUT") or True
+MAX_QUESTIONS_PER_PAGE = os.environ.get("MAX_QUESTIONS_PER_PAGE") or 10
+QUESTIONS_PER_PAGE =  os.environ.get("QUESTIONS_PER_PAGE") or 10
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")) or False
