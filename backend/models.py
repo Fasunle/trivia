e774b3ce84c2c1cd1c021436935eebfebc51899b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-18 08:08:04 +0100

    feat: make configuration modular

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,5 @@
-import os
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-import json
-
-database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -14,9 +9,7 @@ setup_db(app)
 """
 
 
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+def setup_db(app):
     db.app = app
     db.init_app(app)
     db.create_all()
