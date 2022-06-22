d4ef28bf13da715cc60437feadfa60a7afaf08c5
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-06-22 16:23:37 +0100

    fix: cast strings to number

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@ import os
 
 ALLOWED_LIST = os.environ.get("ALLOWED_LIST") or ["http://localhost:3000"]
 ERROR_OUT = os.environ.get("ERROR_OUT") or True
-MAX_QUESTIONS_PER_PAGE = os.environ.get("MAX_QUESTIONS_PER_PAGE") or 10
-QUESTIONS_PER_PAGE =  os.environ.get("QUESTIONS_PER_PAGE") or 10
+MAX_QUESTIONS_PER_PAGE = int(os.environ.get("MAX_QUESTIONS_PER_PAGE")) or 10
+QUESTIONS_PER_PAGE =  int(os.environ.get("QUESTIONS_PER_PAGE")) or 10
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")) or False
